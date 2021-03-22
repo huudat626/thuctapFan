2d78901f4a4e072483e114fdb93d32fa23693a22
huudat626 <huudat626@gmail.com>
2021-03-22 09:45:03 +0700

    add layout

---
+++ thuctapFan/src/app/modules/layouts/layouts.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LayoutsComponent } from './layouts.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [LayoutsComponent]
+})
+export class LayoutsModule { }
